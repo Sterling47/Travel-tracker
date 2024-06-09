ab9f36a14281e7f5ff7b315afc1e3a36ca7fde26
Adam Konber <konber3@gmail.com>
2024-06-09 13:57:42 -0600

    corrected functionality that calcutes total amount spent this year

--- src/dom.js
+++ src/dom.js
@@ -1,8 +1,11 @@
 export const renderTrips = (trips, container, destinationsData) => {
     let totalAmount = 0;
-    
+    const currentDate = new Date('2022/06/12');
+    const currentYear = currentDate.getFullYear()
     trips.forEach(trip => {
         const destination = destinationsData.destinations.find(dest => dest.id === trip.destinationID);
+        const tripDate = new Date(trip.date)
+        const tripYear = tripDate.getFullYear();
         console.log('destination:::', destination)
         const tripElement = document.createElement('div');
         tripElement.classList.add('img-bar')
@@ -15,9 +18,9 @@ export const renderTrips = (trips, container, destinationsData) => {
         `;
         container.appendChild(tripElement);
         console.log('trip duration::', trip.duration)  
-        if (trip.status === 'approved') {
+        if (trip.status === 'approved' && tripYear === currentYear) {
             totalAmount += (trip.duration * destination.estimatedLodgingCostPerDay) 
-            totalAmount += (trip.travelers * destination.estimatedFlightCostPerPerson);
+            totalAmount+= (trip.travelers * destination.estimatedFlightCostPerPerson);
         }
     });
 
