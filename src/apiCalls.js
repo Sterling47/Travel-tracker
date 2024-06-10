a156f7bd1d2d732c99b22a7f34738bc630c3c86f
Adam Konber <konber3@gmail.com>
2024-06-10 14:05:41 -0600

    Completed POST request and displayNewPendingTrip function

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,3 +1,5 @@
+// import { displayNewPendingTrip } from "./dom";
+
 import { displayNewPendingTrip } from "./dom";
 
 export function fetchData() {
@@ -22,14 +24,43 @@ export function fetchDestinations() {
 
 // POST
 
-const bookNewTrip = () => {
-    return fetchData('http://localhost:3001/api/v1/trips', {
+const bookNewTrip = (newTripObj) => {
+    return fetch('http://localhost:3001/api/v1/trips', {
         method: 'POST',
         body: JSON.stringify(newTripObj),
         headers: {
             'Content-Type': 'application/json'
         }
     })
-    .then(response => response.json())
-    .then(data => displayNewPendingTrip(data))
+    .then(response => {
+        if(!response.ok){
+            throw new Error('Error Booking Trip!')
+        }
+        return response.json();
+    });
+}
+
+export const submitNewTrip = (e, userId, destinationsData) => {
+   
+    e.preventDefault();
+    const formElement = e.target;
+    const formData = new FormData(formElement);
+    const newTripObj = {
+        id: Date.now(),
+        userID: userId, 
+        destinationID: Number(formData.get('destinations-list')), 
+        travelers: Number(formData.get('num-of-travelers')), 
+        date: formData.get('trip-date').replace(/-/g, '/'),
+        duration: Number(formData.get('duration')), 
+        status: 'pending',
+        suggestedActivities: ['activities']
+    };
+
+    console.log('newTRIPOBJ:::::',newTripObj);
+    bookNewTrip(newTripObj)
+        .then(data => displayNewPendingTrip(data, destinationsData))
+        .catch(error => {
+            console.error('Error:', error);
+        }); 
+    
 }
--- src/dom.js
+++ src/dom.js
@@ -26,10 +26,32 @@ export const renderTrips = (trips, container, destinationsData) => {
     return totalAmount;
 };
 
- export const displayNewPendingTrip = (data) => {
-    const pending = document.getElementById('pending-trips')
-    pending.innerHTML `
-        <h2>your post is on the way</h2>
-        <img src='${data.image}'>
-    `
+export const displayNewPendingTrip = (data, destinationsData) => {
+    const pending = document.getElementById('pending-trips');
+    console.log(destinationsData)
+    console.log('data2', data)
+    const destination = destinationsData.destinations.find(dest => dest.id === data.newTrip.destinationID);
+    console.log("dest data", destination)
+    const tripElement = document.createElement('div')
+    tripElement.classList.add('img-bar')
+    tripElement.innerHTML = `
+        <div class='dest-display'>
+            <img class='destination-img' src='${destination.image}'>
+            <p>${destination.destination}</p> 
+            <p>${data.newTrip.date}</p>
+        </div>
+    `;
+
+    pending.appendChild(tripElement)
+    console.log('trying::', data);
+}
+
+export function destinationOptions(destinationsData) {
+    const destinationSelect = document.getElementById('destinations-list');
+    destinationsData.destinations.forEach(destination => {
+        const option = document.createElement('option');
+        option.value = destination.id;
+        option.text = destination.destination;
+        destinationSelect.appendChild(option);
+    });
 }
